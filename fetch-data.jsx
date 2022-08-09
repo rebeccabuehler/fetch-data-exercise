17c360f4e17d20bb5af95dd356abb98885ac7a24
Rebecca Buehler <you@example.com>
2022-08-09 11:42:14 -0700

    added dataFetchReducer to give action and return state based on dispatch

--- fetch-data.jsx
+++ fetch-data.jsx
@@ -1,46 +1,73 @@
 const useDataApi = (initalUrl, initialData) => {
-    const {useState, useEffect, useReducer} = React;
-    const [url, setUrl] = useState(initalUrl);
+  const { useState, useEffect, useReducer } = React;
+  const [url, setUrl] = useState(initalUrl);
 
-    const [state, dispatch] = useReducer(dataFetchReducer, {
-        isLoading: false,
-        isError: false,
-        data: initialData
-    });
+  const [state, dispatch] = useReducer(dataFetchReducer, {
+    isLoading: false,
+    isError: false,
+    data: initialData,
+  });
 
-    useEffect(() => {
-        let didCancel = false;
-        const fetchData = async () => {
-            dispatch({type: "FETCH_INIT"});
-            try{
-                const result = await axios(url);
-                if(!didCancel) {
-                    dispatch({type: "FETCH_SUCCESS", payload: result.data});
-                }
-            } catch (error) {
-                if(!didCancel) {
-                    dispatch({type: "FETCH_FAILURE"});
-                }
-            }
-        };
-        fetchData();
-        return () => {
-            didCancel = true;
-        };
-    }, [url]);
-    return [state, setUrl];
+  useEffect(() => {
+    let didCancel = false;
+    const fetchData = async () => {
+      dispatch({ type: "FETCH_INIT" });
+      try {
+        const result = await axios(url);
+        if (!didCancel) {
+          dispatch({ type: "FETCH_SUCCESS", payload: result.data });
+        }
+      } catch (error) {
+        if (!didCancel) {
+          dispatch({ type: "FETCH_FAILURE" });
+        }
+      }
+    };
+    fetchData();
+    return () => {
+      didCancel = true;
+    };
+  }, [url]);
+  return [state, setUrl];
 };
 
+const dataFetchReducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_INIT":
+      return {
+        ...state,
+        isLoading: true,
+        isError: false
+      };
+    case "FETCH_SUCCESS":
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        data: action.payload
+      };
+    case "FETCH_FAILURE":
+      return {
+        ...state,
+        isLoading: false,
+        isError: true
+      };
+    default:
+        throw new Error();
+  }
+};
 
 function App() {
-    const {Fragment, useState, useEffect, useReducer} = React;
-    const {query, setQuery} = useState("MIT");
-    const {currentPage, setCurrentPage} = useState(1);
-    const pageSize = 10;
-    const [{data, isLoading, isError}, doFetch] = useDataApi(    "https://hn.algolia.com/api/v1/search?query=MIT",
+  const { Fragment, useState, useEffect, useReducer } = React;
+  const { query, setQuery } = useState("MIT");
+  const { currentPage, setCurrentPage } = useState(1);
+  const pageSize = 10;
+  const [{ data, isLoading, isError }, doFetch] = useDataApi(
+    "https://hn.algolia.com/api/v1/search?query=MIT",
     {
-      hits: []
-    });
+      hits: [],
+    }
+  );
 
-    return;
+  return;
 }
